005e09def312e140f9102beedb8abe5c43a5d713
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-23 16:14:17 +0200

    manejo de variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,46 @@ function App() {
   const [variable, setVariable] = useState<string>("Fernando");
   const alumno = "pardo";
   const alumnos = ["mario", "pardo", "neuza", "yisus", "melina"];
-  const [contador, setContador]=useState<number>(0)
+  const [contador, setContador] = useState<number>(0);
+  const [name, setName] = useState<string>();
+
   const handleVariable = () => {
     setVariable("Mario");
   };
+
+  const handleName=(e:any)=>{
+    setName(e.target.value)
+  }
   return (
     <>
       <p>{variable}</p>
       <button onClick={handleVariable}>Enviar</button>
-      <h2>
-        Contador
-      </h2>
+      <h2>Contador</h2>
       <p>{contador}</p>
       <p>{contador}</p>
       <p>{contador}</p>
-      <button onClick={()=>setContador(contador+1)}>+1</button>
-      <button onClick={()=>setContador(0)}>reset</button>
-
+      <button onClick={() => setContador(contador + 1)}>+1</button>
+      <button onClick={() => setContador(0)}>reset</button>
 
       <p>{alumno}</p>
       <h1>Alumnos</h1>
-      <p>{alumnos.map((nombre, index) => (
-        <li key={index}>
-          {nombre} - {index}
-        </li>
-      ))}</p>
+      <p>
+        {alumnos.map((nombre, index) => (
+          <li key={index}>
+            {nombre} - {index}
+          </li>
+        ))}
+      </p>
 
+      {/* input y texto */}
+      <h2>nombre: {name}</h2>
+
+      <input
+        type="text"
+        value={name}
+        onChange={handleName}
+        placeholder="Escribe un nombre..."
+      />
     </>
   );
 }
