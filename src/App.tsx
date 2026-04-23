1c70c1802d8fd571b4f440002ead4832e15a1a6c
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-23 16:03:24 +0200

    contador

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [variable, setVariable] = useState<String>("Fernando");
+  const [variable, setVariable] = useState<string>("Fernando");
   const alumno = "pardo";
   const alumnos = ["mario", "pardo", "neuza", "yisus", "melina"];
+  const [contador, setContador]=useState<number>(0)
   const handleVariable = () => {
     setVariable("Mario");
   };
@@ -12,6 +13,16 @@ function App() {
     <>
       <p>{variable}</p>
       <button onClick={handleVariable}>Enviar</button>
+      <h2>
+        Contador
+      </h2>
+      <p>{contador}</p>
+      <p>{contador}</p>
+      <p>{contador}</p>
+      <button onClick={()=>setContador(contador+1)}>+1</button>
+      <button onClick={()=>setContador(0)}>reset</button>
+
+
       <p>{alumno}</p>
       <h1>Alumnos</h1>
       <p>{alumnos.map((nombre, index) => (
@@ -19,6 +30,7 @@ function App() {
           {nombre} - {index}
         </li>
       ))}</p>
+
     </>
   );
 }
